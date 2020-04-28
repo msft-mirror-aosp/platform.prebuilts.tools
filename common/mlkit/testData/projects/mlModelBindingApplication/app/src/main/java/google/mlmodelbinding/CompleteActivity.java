5b02978056499860eea4931298a2a5591161a39e
Lei Yu <jackqdyulei@google.com>
2020-04-28 15:19:32 -0700

    Add ml model binding project for benchmark test
    Bug: 155130752
    Test: No need
    Change-Id: Ic44483c595be4c56cf7635c81ffb96a3879330c2

---
+++ common/mlkit/testData/projects/mlModelBindingApplication/app/src/main/java/google/mlmodelbinding/CompleteActivity.java
@@ -0,0 +1,25 @@
+package google.mlmodelbinding;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import google.mlmodelbinding.ml.Model;
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
+
+public class CompleteActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+        try {
+          Efficie<caret>;
+        } catch (Exception e) {
+          // do nothing
+        }
+    }
+}
