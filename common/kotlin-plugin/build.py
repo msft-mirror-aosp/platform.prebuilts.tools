cda403ec290b6af87f05d8225c584eada27d3c2b
Matthew Gharrity <gharrma@google.com>
2022-04-01 18:09:53 +0000

    Kotlin build: sort jars
    ...to avoid unnecessary changes in the Kotlin JPS library definition.
    
    Bug: not filed
    Test: manual
    Change-Id: I833f1ce5953e87e652f845ff8b4778cf1f29889a

--- common/kotlin-plugin/build.py
+++ common/kotlin-plugin/build.py
@@ -192,7 +192,7 @@ def write_jps_lib_xml(args):
     project_dir = args.workspace.joinpath('tools/adt/idea')
 
     # Note: see comment in the BUILD file for why we exclude kotlin-stdlib and kotlin-reflect.
-    jars = list(args.workspace.glob('prebuilts/tools/common/kotlin-plugin/Kotlin/lib/*.jar'))
+    jars = sorted(args.workspace.glob('prebuilts/tools/common/kotlin-plugin/Kotlin/lib/*.jar'))
     jars = [jar for jar in jars if jar.name not in ['kotlinc_kotlin-stdlib.jar', 'kotlinc_kotlin-reflect.jar']]
     jars = [os.path.relpath(jar, project_dir) for jar in jars]
 
