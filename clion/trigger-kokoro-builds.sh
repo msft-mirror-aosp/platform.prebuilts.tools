a74269a744a9d858dc74eb2d0356576e657e577b
Emre Kultursay <emrekultursay@google.com>
2023-02-09 09:15:16 -0800

    Add ability to trigger on latest tag
    This adds a simple way to get the latest tag without downloading the
    repo. I haven't decided how to run this periodically (i.e., borg-cron,
    corp-cron, etc.).
    
    Usage: ./trigger-kokoro-builds.sh latest_idea_tag
    
    Bug: n/a
    Test: n/a
    Change-Id: I89badd8772198b7a2ee9bed05cd13290d159b690

--- clion/trigger-kokoro-builds.sh
+++ clion/trigger-kokoro-builds.sh
@@ -1,15 +1,39 @@
 #!/bin/bash
 
-COMMITISH=$1
+set -e
 
-if [ -z "$COMMITISH" ]; then
+committish="$1"
+
+if [[ -z "${committish}" ]]; then
   cat <<EOF
 Usage:
-  trigger-kokoro-builds.sh <commitish>
+  trigger-kokoro-builds.sh <committish>
+  trigger-kokoro-builds.sh latest_idea_tag
 For example,
   trigger-kokoro-builds.sh idea/201.6073.9
 EOF
   exit 1
+fi
+
+if [[ "${committish}" == "latest_idea_tag" ]]; then
+  repo="team/android-studio/third_party/llvm-project"
+  echo "Finding latest idea tag from repository: '${repo}'"
+  response="$(stubby call --proto2 blade:git GitFS.ListRefs \
+    "repo: '${repo}' ref_prefix: 'refs/tags/idea/'")"
+  tags="$(echo "${response}" \
+    | grep "refs/tags/idea" \
+    | sed 's|^.*refs/tags/\(.*\)"$|\1\n|')"
+  # This assumes that the version numbers embedded in the tags
+  # will be monotonically increasing alphabetically.
+  latest_idea_tag="$(echo "${tags}" \
+    | sort \
+    | tail -1)"
+  echo "Latest idea tag found: '${latest_idea_tag}'"
+  committish="${latest_idea_tag}"
+  if [[ -z "${committish}" ]]; then
+    echo "Failed to find latest idea tag"
+    exit 1
+  fi
 fi
 
 echo "Triggering Windows build..."
@@ -18,7 +42,7 @@ full_job_name: "android-studio/clangd/win/release"
 multi_scm_revision {
   git_on_borg_scm_revision {
     name: "llvm-project"
-    sha1: "$1"
+    sha1: "${committish}"
   }
 }
 EOF
@@ -29,7 +53,7 @@ full_job_name: "android-studio/clangd/mac/release"
 multi_scm_revision {
   git_on_borg_scm_revision {
     name: "llvm-project"
-    sha1: "$1"
+    sha1: "${committish}"
   }
 }
 EOF
@@ -40,7 +64,7 @@ full_job_name: "android-studio/clangd/linux/release"
 multi_scm_revision {
   git_on_borg_scm_revision {
     name: "llvm-project"
-    sha1: "$1"
+    sha1: "${committish}"
   }
 }
 EOF
