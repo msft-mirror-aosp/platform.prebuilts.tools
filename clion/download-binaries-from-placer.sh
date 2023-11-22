08d207b8b89ae4096f7b60f14220446110a5a6d6
Emre Kultursay <emrekultursay@google.com>
2023-11-22 09:21:44 -0800

    Add split Mac builds to trigger/download scripts
    The trigger script now also triggers the single-abi arm64/x86_64
    MacOS builds.
    
    The download script now also takes --mac_arm64/--mac_x86_64 args
    for single-abi builds. It puts them under the mac/{abi}/ folder.
    
    Bug: 284389445
    Test: Ran both scripts at latest idea tag
    Change-Id: Ib55cae10370a9e1656fb5e8e60e2656611d1cc05

--- clion/download-binaries-from-placer.sh
+++ clion/download-binaries-from-placer.sh
@@ -5,7 +5,7 @@
 #
 # Usage:
 #
-#   download-binaries-from-placer.sh [--nostrip] [--linux <build#>] [--mac <build#>] [--win <build#>]
+#   download-binaries-from-placer.sh [--nostrip] [--linux <build#>] [--mac <build#>] [--mac_arm64 <build#>] [--mac_x86_64 <build#>] [--win <build#>]
 #
 # To find the release number, go to http://go/as-clangd-kokoro and find the
 # successful builds that you have triggered.
@@ -27,7 +27,25 @@ while (( "$#" )); do
       ;;
     --mac)
       if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
-        MAC_RELEASE="$2"
+        MAC_UNIVERSAL_RELEASE="$2"
+        shift 2
+      else
+        echo "Error: Argument for $1 is missing" >&2
+        exit 1
+      fi
+      ;;
+    --mac_arm64)
+      if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+        MAC_ARM64_RELEASE="$2"
+        shift 2
+      else
+        echo "Error: Argument for $1 is missing" >&2
+        exit 1
+      fi
+      ;;
+    --mac_x86_64)
+      if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+        MAC_X86_64_RELEASE="$2"
         shift 2
       else
         echo "Error: Argument for $1 is missing" >&2
@@ -48,13 +66,14 @@ while (( "$#" )); do
         shift 1
       ;;
     -*|--*=)
-      echo "Usage: download-binaries-from-placer.sh [--nostrip] [--linux <build#>] [--mac <build#>] [--win <build#>]" >&2
+      echo "Usage: download-binaries-from-placer.sh [--nostrip] [--linux <build#>] [--mac <build#>] [--mac_arm64 <build#>] [--mac_x86_64 <build#>] [--win <build#>]" >&2
       exit 1
       ;;
   esac
 done
 
 readonly BIN_DIR="${SCRIPT_DIR}/bin/clang"
+
 if [[ -n "${LINUX_RELEASE}" ]]; then
   rm -rv "${BIN_DIR}/linux"
   mkdir -pv "${BIN_DIR}/linux/x64"
@@ -67,13 +86,35 @@ if [[ -n "${LINUX_RELEASE}" ]]; then
     echo "Stripping is disabled"
   fi
 fi
-if [[ -n "${MAC_RELEASE}" ]]; then
-  rm -rv "${BIN_DIR}/mac"
+
+if [[ -n "${MAC_UNIVERSAL_RELEASE}" ]]; then
+  # Do not delete the entire mac/ directory here, as it has aarch64/x64
+  # subdirectories that should be controlled using other switches.
+  rm -rv "${BIN_DIR}/mac/clangd"
   mkdir -pv "${BIN_DIR}/mac"
-  fileutil cp -R -resume "/placer/prod/home/kokoro-dedicated/build_artifacts/prod/android-studio/clangd/mac/release/${MAC_RELEASE}/*/*" "${BIN_DIR}/mac"
+  fileutil cp -R -resume "/placer/prod/home/kokoro-dedicated/build_artifacts/prod/android-studio/clangd/mac/release/${MAC_UNIVERSAL_RELEASE}/*/*" "${BIN_DIR}/mac"
   rm -v "${BIN_DIR}/mac"/*.intoto.jsonl
   chmod -v +x "${BIN_DIR}/mac"/clangd
 fi
+
+# Build has name mac_arm64, but the directory is called mac/aarch64.
+if [[ -n "${MAC_ARM64_RELEASE}" ]]; then
+  rm -rv "${BIN_DIR}/mac/aarch64"
+  mkdir -pv "${BIN_DIR}/mac/aarch64"
+  fileutil cp -R -resume "/placer/prod/home/kokoro-dedicated/build_artifacts/prod/android-studio/clangd/mac_arm64/release/${MAC_ARM64_RELEASE}/*/*" "${BIN_DIR}/mac/aarch64"
+  rm -v "${BIN_DIR}/mac/aarch64"/*.intoto.jsonl
+  chmod -v +x "${BIN_DIR}/mac/aarch64"/clangd
+fi
+
+# Build has name mac_x86_64, but the directory is called mac/x64.
+if [[ -n "${MAC_X86_64_RELEASE}" ]]; then
+  rm -rv "${BIN_DIR}/mac/x64"
+  mkdir -pv "${BIN_DIR}/mac/x64"
+  fileutil cp -R -resume "/placer/prod/home/kokoro-dedicated/build_artifacts/prod/android-studio/clangd/mac_x86_64/release/${MAC_X86_64_RELEASE}/*/*" "${BIN_DIR}/mac/x64"
+  rm -v "${BIN_DIR}/mac/x64"/*.intoto.jsonl
+  chmod -v +x "${BIN_DIR}/mac/x64"/clangd
+fi
+
 if [[ -n "${WIN_RELEASE}" ]]; then
   rm -rv "${BIN_DIR}/win"
   mkdir -pv "${BIN_DIR}/win/x64"
