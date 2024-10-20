6728e94e030ab93339defdd8c1434de050c0fdda
Jinseong Jeon <jsjeon@google.com>
2024-10-19 22:38:38 -0700

    Lower the language versions
    ...so that external devs can still use kotlinc 1.9
    and/or other plugins that are not ready for K2 yet.
    
    Bug: 367868888
    Bug: 373243841
    Change-Id: Ie931712992e699179946df0715668f5ebcd72102

--- common/lint-psi/build.gradle.kts
+++ common/lint-psi/build.gradle.kts
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("java")
-    kotlin("jvm") version "2.0.0" // Aim to match the Kotlin version below.
+    kotlin("jvm") version "1.9.0" // Aim to match the Kotlin version below.
 }
 
 val intellijVersion = getEnvOrError("INTELLIJ_VERSION")
@@ -144,6 +144,8 @@ tasks.withType<KotlinCompile> {
             "-Xcontext-receivers",
             "-opt-in=org.jetbrains.kotlin.analysis.api.permissions.KaAllowProhibitedAnalyzeFromWriteAction",
             "-opt-in=org.jetbrains.kotlin.analysis.api.KaIdeApi",
+            "-api-version=1.9",      // Until the entire build target is 2.0?
+            "-language-version=1.9", // Until the entire build target is 2.0?
         )
         suppressWarnings = true
     }
